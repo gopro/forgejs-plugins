4ad6e5022e32a7d10646b45ba7794c1f7b4a24b1
Raphaël Roux <rroux@gopro.com>
2017-02-15 09:54:54 +0100

    update the FPS plugin with dragable container and mode swith + mode option

--- FPS/src/FPS.js
+++ FPS/src/FPS.js
@@ -2,16 +2,45 @@ var ForgePlugins = ForgePlugins || {};
 
 ForgePlugins.FPS = function()
 {
+    this._container = null;
     this._stats = null;
+    this._mode = 0;
+
+    this._modes = ["fps", "ms", "mb"];
 };
 
 ForgePlugins.FPS.prototype =
 {
     boot: function()
     {
-        this._stats = new Stats();
-        this.plugin.container.dom.appendChild(this._stats.dom);
+        this._container = this.plugin.create.displayObjectContainer();
 
+        var pixelRatio = Math.round( window.devicePixelRatio || 1 );
+        this._container.width = 80 * pixelRatio;
+        this._container.height = 48 * pixelRatio;
+        this._container.pointer.cursor = "pointer";
+        this._container.drag.enabled = true;
+        this._container.pointer.onClick.add(this._clickHandler, this);
+        this.plugin.container.addChild(this._container);
+
+        this._stats = new Stats();
+        this._stats.dom.style.position = "relative";
+        this._stats.dom.style.pointerEvents = "none";
+        this._container.dom.appendChild(this._stats.dom);
+
+        var mode = this.plugin.options.mode;
+
+        if(typeof mode === "string")
+        {
+            mode = this._modes.indexOf(mode);
+        }
+
+        if(typeof mode === "number" && mode < this._modes.length && mode >= 0)
+        {
+            this._mode = mode
+        }
+
+        this._stats.showPanel(this._mode);
         this._stats.begin();
     },
 
@@ -33,7 +62,12 @@ ForgePlugins.FPS.prototype =
 
     destroy: function()
     {
-        this.plugin.container.dom.removeChild(this._stats.dom);
+        this._container = null;
         this._stats = null;
+    },
+
+    _clickHandler: function()
+    {
+        this._stats.showPanel(++ this._mode % this._stats.dom.children.length);
     }
 };
