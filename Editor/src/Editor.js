4a1742e240b6b69da12ce166ec5e084d3b96e44c
Raphaël Roux <rroux@gopro.com>
2017-06-09 10:03:11 +0200

    Add history  + save/load features WIP commit

--- Editor/src/Editor.js
+++ Editor/src/Editor.js
@@ -1,24 +1,116 @@
 
 var ForgePlugins = ForgePlugins || {};
 
-/**
- */
 ForgePlugins.Editor = function()
 {
     this._ui = null;
 
+    this._count = 0;
+
     this._history = null;
+
+    this._selected = null;
+
+    this.onSelected = new FORGE.EventDispatcher(this);
+};
+
+ForgePlugins.Editor.HOTSPOT_DEFAULT_CONFIG =
+{
+    facingCenter: true
 };
 
 ForgePlugins.Editor.prototype =
 {
-    /**
-     * The boot function, initialize the container of the flags, and then the
-     * flags.
-     */
     boot: function()
     {
         this._ui = new ForgePlugins.EditorUI(this);
+        this._history = new ForgePlugins.EditorHistory(this);
+        this._history.add();
+    },
+
+    add: function(config, history)
+    {
+        if(typeof config === "undefined" || config === null)
+        {
+            config = this._generateHotspotConfig();
+        }
+
+        this.viewer.hotspots.create(config);
+        this.reset();
+        this.selected = config.uid;
+
+        if(history !== false)
+        {
+            this._history.add("hotspot add");
+        }
+    },
+
+    delete: function(uid, history)
+    {
+        this.viewer.hotspots.remove(uid);
+        this.reset();
+
+        this.selected = null;
+
+        if(history !== false)
+        {
+            this._history.add();
+        }
+    },
+
+    save: function()
+    {
+        var dump = this.dump();
+        var json = JSON.stringify(dump, null, 4);
+        var blob = new Blob([json], {type: "application/json"});
+        var url  = URL.createObjectURL(blob);
+
+        var a = document.createElement('a');
+        a.download = "hotspots.json";
+        a.href = url;
+        a.click();
+    },
+
+    load: function(hotspots)
+    {
+        this.viewer.hotspots.clear();
+        this.clear();
+
+        for(var i = 0, ii = hotspots.length; i < ii; i++)
+        {
+            this.viewer.hotspots.create(hotspots[i]);
+        }
+
+        this.populate();
+    },
+
+    clear: function()
+    {
+        this.viewer.renderer.objects.clear();
+    },
+
+    populate: function()
+    {
+        this.viewer.renderer.objects.createRenderScenes();
+        this.viewer.renderer.renderPipeline.addRenderScenes(viewer.renderer.objects.renderScenes);
+    },
+
+    reset: function()
+    {
+        this.clear();
+        this.populate();
+    },
+
+    lookAt: function(uid)
+    {
+        var hs = FORGE.UID.get(uid);
+
+        if(FORGE.Utils.isTypeOf(hs, "Hotspot3D"))
+        {
+            var pos = hs.transform.position;
+            var spherical = FORGE.Math.cartesianToSpherical(pos.x, pos.y, pos.z, "deg");
+            this.viewer.camera.lookAt(spherical.theta, spherical.phi, 0, this.viewer.camera.fov, 100);
+        }
     },
 
     update: function()
@@ -26,12 +118,60 @@ ForgePlugins.Editor.prototype =
 
     },
 
-    /**
-     * Destroy the container and its flags
-     */
+    dump: function()
+    {
+        var dump = this.viewer.hotspots.dump();
+        return dump;
+    },
+
     destroy: function()
     {
         this._ui.destroy;
         this._ui = null;
+    },
+
+    _generateHotspotConfig: function()
+    {
+        var config = FORGE.Utils.extendSimpleObject({}, ForgePlugins.Editor.HOTSPOT_DEFAULT_CONFIG);
+        config.uid = "hotspot-3d-editor-" + this._count;
+        config.transform = { position: { theta: viewer.camera.yaw, phi: viewer.camera.pitch } };
+        this._count++;
+
+        return config;
     }
 };
+
+Object.defineProperty(ForgePlugins.Editor.prototype, "history",
+{
+    get: function()
+    {
+        return this._history;
+    },
+});
+
+Object.defineProperty(ForgePlugins.Editor.prototype, "selected",
+{
+    get: function()
+    {
+        return this._selected;
+    },
+
+    set: function(value)
+    {
+        if(value === null)
+        {
+            this._selected = null;
+            this.onSelected.dispatch();
+            return;
+        }
+
+        var hs = FORGE.UID.get(value);
+
+        if(FORGE.Utils.isTypeOf(hs, "Hotspot3D"))
+        {
+            this._selected = value;
+            this.lookAt(this._selected);
+            this.onSelected.dispatch();
+        }
+    }
+});
--- Editor/src/EditorUI.js
+++ Editor/src/EditorUI.js
@@ -1,8 +1,6 @@
 
 var ForgePlugins = ForgePlugins || {};
 
-/**
- */
 ForgePlugins.EditorUI = function(editor)
 {
     this._editor = editor;
@@ -11,33 +9,181 @@ ForgePlugins.EditorUI = function(editor)
 
     this._addButton = null;
 
+    this._deleteButton = null;
+
+    this._undoButton = null;
+
+    this._redoButton = null;
+
+    this._saveButton = null;
+
+    this._loadInput = null;
+
+    this._loadButton = null;
+
+    this._loadReader = null;
+
+    this._hotspotList = null;
+
     this._boot();
 };
 
 ForgePlugins.EditorUI.prototype =
 {
-    /**
-     * The boot function, initialize the container of the flags, and then the
-     * flags.
-     */
     _boot: function()
     {
-        // Create a root container and place it on the scene
-        this._container = this._editor.plugin.create.displayObjectContainer();
-        this._container.background = "white";
-        this._container.width = 300;
-        this._container.height = 600;
-
-        this._container.pointer.enabled = true;
-
-        // Add the container to the scene
-        this._editor.plugin.container.addChild(this._container);
-
+        this._container = document.createElement("div");
+        this._container.id = "editor-main-container";
+        this._editor.plugin.container.dom.appendChild(this._container);
 
         this._addButton = document.createElement("button");
-        this._addButton.innerHTML = "Add Hotspot";
+        this._addButton.id = "hotspot-add-button";
+        this._addButton.innerHTML = "Add";
+        this._addButton.addEventListener("click", this._addButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._addButton);
 
-        this._container.dom.appendChild(this._addButton);
+        this._deleteButton = document.createElement("button");
+        this._deleteButton.id = "hotspot-delete-button";
+        this._deleteButton.innerHTML = "Delete";
+        this._deleteButton.disabled = true;
+        this._deleteButton.addEventListener("click", this._deleteButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._deleteButton);
+
+        this._undoButton = document.createElement("button");
+        this._undoButton.id = "hotspot-undo-button";
+        this._undoButton.innerHTML = "Undo";
+        this._undoButton.addEventListener("click", this._undoButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._undoButton);
+
+        this._redoButton = document.createElement("button");
+        this._redoButton.id = "hotspot-redo-button";
+        this._redoButton.innerHTML = "Redo";
+        this._redoButton.addEventListener("click", this._redoButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._redoButton);
+
+        this._saveButton = document.createElement("button");
+        this._saveButton.id = "hotspot-save-button";
+        this._saveButton.innerHTML = "Save";
+        this._saveButton.addEventListener("click", this._saveButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._saveButton);
+
+        this._loadInput = document.createElement("input");
+        this._loadInput.type = "file";
+        this._loadInput.name = "file";
+        this._loadInput.id = "hotspot-load-input";
+        this._loadInput.style.display = "none";
+        this._loadInput.addEventListener('change', this._loadFileSelectHandler.bind(this), false);
+        this._container.appendChild(this._loadInput);
+
+        this._loadButton = document.createElement("button");
+        this._loadButton.id = "hotspot-load-button";
+        this._loadButton.innerHTML = "Load";
+        this._loadButton.addEventListener("click", this._loadButtonClickHandler.bind(this), false);
+        this._container.appendChild(this._loadButton);
+
+        this._loadReader = new FileReader();
+        this._loadReader.onload = this._loadReaderLoadHandler.bind(this);
+
+        this._hotspotList = document.createElement("div");
+        this._hotspotList.id = "hotspots-list";
+        this._container.appendChild(this._hotspotList);
+
+        this._updateList();
+
+        this._editor.onSelected.add(this._onSelectedHandler, this);
+    },
+
+    _addButtonClickHandler: function(event)
+    {
+        this._editor.add();
+        this._updateList();
+    },
+
+    _deleteButtonClickHandler: function(event)
+    {
+        this._editor.delete(this._editor.selected);
+        this._updateList();
+    },
+
+    _undoButtonClickHandler: function(event)
+    {
+        this._editor.history.undo();
+        this._updateList();
+    },
+
+    _redoButtonClickHandler: function(event)
+    {
+        this._editor.history.redo();
+        this._updateList();
+    },
+
+    _saveButtonClickHandler: function(event)
+    {
+        this._editor.save();
+    },
+
+    _loadButtonClickHandler: function(event)
+    {
+        this._loadInput.click();
+    },
+
+    _loadFileSelectHandler: function(event)
+    {
+        var files = event.target.files;
+        var file = files[0];
+
+        if(typeof file !== "undefined" && file.type.match("application/json"))
+        {
+            console.log("json file ok");
+            this._loadReader.readAsText(file);
+        }
+    },
+
+    _loadReaderLoadHandler: function(event)
+    {
+        console.log(event);
+        var data = JSON.parse(event.target.result);
+        this._editor.load(data);
+    },
+
+    _onSelectedHandler: function()
+    {
+        this._deleteButton.disabled = this._editor.selected === null ? true : false;
+        this._updateList();
+    },
+
+    _updateList: function()
+    {
+        this._clearList();
+
+        var viewer = this._editor.viewer;
+        var hotspots = viewer.hotspots.all;
+
+        var hs, p;
+        for(var i = 0, ii = hotspots.length; i < ii; i++)
+        {
+            hs = hotspots[i];
+            p = document.createElement("p");
+            p.addEventListener("click", this._hotspotListClickHandler.bind(this));
+            p.innerHTML = hs.uid;
+
+            if(this._editor.selected === hs.uid)
+            {
+                p.className = "selected";
+            }
+
+            this._hotspotList.appendChild(p);
+        }
+    },
+
+    _clearList: function()
+    {
+        this._hotspotList.innerHTML = "";
+    },
+
+    _hotspotListClickHandler: function(event)
+    {
+        this._editor.selected = event.target.innerHTML;
     },
 
     update: function()
@@ -45,9 +191,6 @@ ForgePlugins.EditorUI.prototype =
 
     },
 
-    /**
-     * Destroy the container and its flags
-     */
     destroy: function()
     {
         this._container = null;
