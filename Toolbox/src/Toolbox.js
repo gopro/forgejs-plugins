2ffc2855102d0a308b2bba5bc46dc0cd3312c68a
Raphaël Roux <rroux@gopro.com>
2017-07-25 14:00:25 +0200

    Add options to Toolbax plugin + add a controllers section to toggle drag

--- Toolbox/src/Toolbox.js
+++ Toolbox/src/Toolbox.js
@@ -13,11 +13,22 @@ ForgePlugins.Toolbox = function()
 
     this._scene = null;
 
+    this._controllers = null;
+
     this._canvas = null;
 
-    this._options =
+    this._options = null;
+};
+
+ForgePlugins.Toolbox.DEFAULT_OPTIONS =
+{
+    pannels:
     {
-        cross: true
+        camera: { open: false, options: { cross: false } },
+        view: { open: false },
+        story: { open: false },
+        scene : { open: false },
+        controllers: { open: true }
     }
 };
 
@@ -28,18 +39,41 @@ ForgePlugins.Toolbox.prototype =
      */
     boot: function()
     {
+        this._options = FORGE.Utils.extendSimpleObject(ForgePlugins.Toolbox.DEFAULT_OPTIONS, this.plugin.options, true);
+        console.log(this._options)
+
         this._canvas = this.plugin.create.canvas();
         this._canvas.maximize(true);
         this.plugin.container.addChild(this._canvas);
 
         this._gui = new dat.GUI();
 
-        this._gui.add(FORGE, "VERSION");
+        this._gui.add(FORGE, "VERSION").domElement.firstChild.disabled = true;
 
-        this._addCamera();
-        this._addView();
-        this._addStory();
-        this._addScene();
+        if(typeof this._options.pannels.camera === "object")
+        {
+            this._addCamera();
+        }
+
+        if(typeof this._options.pannels.view === "object")
+        {
+            this._addView();
+        }
+
+        if(typeof this._options.pannels.story === "object")
+        {
+            this._addStory();
+        }
+
+        if(typeof this._options.pannels.scene === "object")
+        {
+            this._addScene();
+        }
+
+        if(typeof this._options.pannels.controllers === "object")
+        {
+            this._addControllers();
+        }
 
         this.viewer.story.onSceneLoadComplete.add(this._onSceneLoadCompleteHandler, this);
     },
@@ -55,13 +89,23 @@ ForgePlugins.Toolbox.prototype =
         this._camera.add(this.viewer.camera, "fov").listen();
         this._camera.add(this.viewer.camera, "parallax", 0, 1).listen();
 
-        this._camera.add(this._options, "cross").name("cross");
+        this._camera.add(this._options.pannels.camera.options, "cross").name("cross");
+
+        if(this._options.pannels.camera.open === true)
+        {
+            this._camera.open();
+        }
     },
 
     _addView: function()
     {
         this._view = this._gui.addFolder("View");
         this._view.add(this.viewer.renderer.view, "type", ["rectilinear", "gopro", "flat"]).listen();
+
+        if(this._options.pannels.view.open === true)
+        {
+            this._view.open();
+        }
     },
 
     _addStory: function()
@@ -74,6 +118,10 @@ ForgePlugins.Toolbox.prototype =
             this._story.add(this.viewer.story, "groupUid", this.viewer.story.groupUids).name("group").listen();
         }
 
+        if(this._options.pannels.story.open === true)
+        {
+            this._story.open();
+        }
     },
 
     _addScene: function()
@@ -83,6 +131,26 @@ ForgePlugins.Toolbox.prototype =
         this._scene.add(this.viewer.story.scene, "name");
         this._scene.add(this.viewer.story.scene, "slug");
         this._scene.add(this.viewer.story.scene, "viewCount");
+
+        if(this._options.pannels.scene.open === true)
+        {
+            this._scene.open();
+        }
+    },
+
+    _addControllers: function()
+    {
+        this._controllers = this._gui.addFolder("Controllers");
+
+        if(viewer.controllers.getByType("pointer") !== null)
+        {
+            this._controllers.add(viewer.controllers.getByType("pointer").orientation, "drag");
+        }
+
+        if(this._options.pannels.controllers.open === true)
+        {
+            this._controllers.open();
+        }
     },
 
     _onSceneLoadCompleteHandler: function()
@@ -98,23 +166,36 @@ ForgePlugins.Toolbox.prototype =
         this._scene.updateDisplay();
     },
 
-    update: function()
+    _clearCanvas: function()
     {
         var ctx = this._canvas.context2D;
         ctx.clearRect(0, 0, this._canvas.pixelWidth, this._canvas.pixelHeight);
+    },
 
-        if(this._options.cross === true)
+    _drawCross: function()
+    {
+        var ctx = this._canvas.context2D;
+        var center = { x: this._canvas.pixelWidth / 2, y: this._canvas.pixelHeight / 2 };
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "white";
+        ctx.beginPath();
+        ctx.moveTo(center.x - 10, center.y);
+        ctx.lineTo(center.x + 10, center.y);
+        ctx.moveTo(center.x, center.y - 10);
+        ctx.lineTo(center.x, center.y + 10);
+        ctx.stroke();
+    },
+
+    update: function()
+    {
+        this._clearCanvas();
+
+        var camera = this._options.pannels.camera;
+
+        if(typeof camera === "object" && camera.options.cross === true)
         {
-            var center = { x: this._canvas.pixelWidth / 2, y: this._canvas.pixelHeight / 2 };
-
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "white";
-            ctx.beginPath();
-            ctx.moveTo(center.x - 10, center.y);
-            ctx.lineTo(center.x + 10, center.y);
-            ctx.moveTo(center.x, center.y - 10);
-            ctx.lineTo(center.x, center.y + 10);
-            ctx.stroke();
+            this._drawCross();
         }
     },
 
